78bf560816371a37372589ca82f7a71fb6c0914c
tarık <kritatc.kont@gmail.com>
2023-09-06 01:18:44 +0300

    v

--- src/main/java/githubStudy.java
+++ src/main/java/githubStudy.java
@@ -1,7 +1,8 @@
 public class githubStudy {
     public static void main(String[] args) {
 
-       toplam(5,9);
+        System.out.println("yeni bilgi");
+
     }
 
     public static void toplam(int a,int b){
