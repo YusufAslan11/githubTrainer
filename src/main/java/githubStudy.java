a51ea58505d6f6e488635b88bee2e4b533290848
Yusuf Aslan <117074474+YusufAslan11@users.noreply.github.com>
2023-09-06 00:53:33 +0300

    v1

---
+++ src/main/java/githubStudy.java
@@ -0,0 +1,11 @@
+public class githubStudy {
+    public static void main(String[] args) {
+
+       toplam(5,9);
+    }
+
+    public static void toplam(int a,int b){
+
+        System.out.println(a+b);
+
+    }}
